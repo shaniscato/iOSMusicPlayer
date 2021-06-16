212b402c7cdd90f2b296638532b3950668b18f14
Shanis Cato <shanis.cato@outlook.com>
2021-06-16 10:10:32 -0700

    Initial Commit

---
+++ MusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by student on 16/06/2021.
+//  Copyright © 2021 swen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
